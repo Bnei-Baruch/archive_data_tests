60fcc9f563bee86a2cc18e7256544aacca5f6ae2
Samuel Shapiro <sshapiro@tlv-mp67p.telaviv.corp.akamai.com>
2017-10-29 12:15:15 +0200

    Additional test cases

--- data_verifyer.py
+++ data_verifyer.py
@@ -6,7 +6,12 @@
 import requests
 import logging
 
+import sys
+
+from urllib3.exceptions import ProtocolError
+
 BACKEND_ENDPOINT = "https://archive.kbb1.com/backend/content_units"
+CDN_ENDPOINT = "https://cdn.kabbalahmedia.info/"
 CONTENT_UNIT_URL = "https://archive.kbb1.com/en/programs/full/"
 PAGE_SIZE = 1000
 
@@ -16,7 +21,7 @@ def get_total():
     return r.json()['total']
 
 
-def fetch_content_unts(page_no=1, page_size=PAGE_SIZE):
+def fetch_content_units(page_no=1, page_size=PAGE_SIZE):
     r = requests.get(BACKEND_ENDPOINT, params={'page_no': str(page_no), 'page_size': str(page_size), "language": 'en'})
     return r.json()['content_units']
 
@@ -34,7 +39,50 @@ def fetch_content_unit_files_data(cu_id, lang="en"):
     if not cu_id:
         raise ValueError("Emtpy content unit ID")
     r = requests.get(BACKEND_ENDPOINT + "/" + cu_id, params={"language": lang})
-    return r.json()['files']
+    return r.status_code, r.json()['files']
+
+
+def check_cdn_file_url(file_id):
+    # first we need to get the real url to file we're redirected to
+    file_url = CDN_ENDPOINT + file_id
+    r = requests.get(file_url, allow_redirects=False)
+    # getting real url and just trying to access without downloading it
+    file_url = r.headers['location']
+    r = requests.head(file_url)
+    return r.status_code, file_url
+
+
+def test_fetch_all_content_units(logger):
+    total_pages = get_total() // PAGE_SIZE
+    cu_ids = []
+
+    for page in range(1, total_pages):
+        print("--------------- PAGE: {} -----------------".format(page))
+        try:
+            for cu_id in fetch_content_units(page, PAGE_SIZE):
+                cu_ids.append(cu_id['id'])
+        except ConnectionError as cerr:
+            logger.error("{} on wile fetching page #{}".format(cerr.strerror, page))
+            pass
+    return cu_ids
+
+
+def test_fetch_content_units_file_data(logger, cu_ids):
+    file_guids = []
+    for cu_id in cu_ids:
+        try:
+            status_code, files_data = fetch_content_unit_files_data(cu_id)
+            if status_code != 200:
+                logger.error("Error accessing {}{} - status code: {}".format(BACKEND_ENDPOINT, cu_id, status_code))
+                continue
+            for file in files_data:
+                print("File: {}".format(file))
+                file_guids.append(file)
+        except ConnectionResetError as cerr:
+            logger.error("{} while fetching content unit {}".format(cerr, BACKEND_ENDPOINT + "/" +
+                                                                    cu_id))
+            continue
+    return file_guids
 
 
 def run_content_units_test(logger):
@@ -43,7 +91,7 @@ def run_content_units_test(logger):
     for page in range(1, total_pages):
         print("--------------- PAGE: {} -----------------".format(page))
         try:
-            cu_ids = fetch_cu_ids(fetch_content_unts(page, PAGE_SIZE))
+            cu_ids = fetch_cu_ids(fetch_content_units(page, PAGE_SIZE))
             for cu_id in cu_ids:
                 try:
                     files_data = fetch_content_unit_files_data(cu_id)
@@ -52,7 +100,10 @@ def run_content_units_test(logger):
                                                                             cu_id))
                     continue
                 for file in files_data:
-                    print("File: {}".format(file['name']))
+                    status_code, file_url = check_cdn_file_url(file['id'])
+                    if status_code != 302:
+                        logger.error("Error accessing {} - status code: {}".format(file_url, status_code))
+                    print("File: {} - URL: {} - Status code: {}".format(file['name'], file_url, status_code))
         except ConnectionError as cerr:
             logger.error("{} on wile fetching page #{}".format(cerr.strerror, page))
             pass
@@ -66,8 +117,17 @@ def main():
     logger.addHandler(hdlr)
     logger.setLevel(logging.WARNING)
 
-    run_content_units_test(logger)
+    tests = {
+        "fetch_countent_units": fetch_content_units()
+    }
+    # run_content_units_test(logger)
+    cu_ids = test_fetch_all_content_units(logger)
+    test_fetch_content_units_file_data(logger, cu_ids)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Test stopped by user ...")
+        sys.exit(0)
